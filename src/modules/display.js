54472e37c1e799dd89a5c30f749a24f56ce461a9
BenM4k <103831502+BenM4k@users.noreply.github.com>
2023-05-14 12:40:36 +0200

    initial setup

---
+++ src/modules/display.js
@@ -0,0 +1,18 @@
+function display(persons, table){
+    persons.forEach(person =>{
+        const list = document.createElement('li');
+        const name = document.createElement('p');
+        const score = document.createElement('span');
+    
+        list.className = 'score__list';
+        name.innerText = `${person.name}:`;
+        score.innerText = `${person.score}`;
+    
+        list.appendChild(name);
+        list.appendChild(score);
+
+        table.appendChild(list);
+    })
+}
+
+export default display;
